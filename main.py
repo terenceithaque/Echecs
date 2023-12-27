60da24bcb783893df4bedb09e68706ec45d635f8
Terence Le Thierry <terence.lethierry@protonmail.com>
2023-12-27 19:13:07 +0100

    Ajout d'une fonction dans main.py pour demander au joueur s'il souhaite quitter le jeu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Script principal du jeu
 import pygame
+from tkinter import messagebox
+
 pygame.init() # Démarrer le module pygame
 
 largeur = 800 # Largeur de l'écran
@@ -10,8 +12,18 @@ pygame.display.set_caption("Echecs !")
 
 running = True # On crée une variable pour obtenir l'état actuel de l'exécution du jeu.
 
+
+def quitter_jeu():
+    "Demander au joueur s'il souhaite quitter le jeu"
+    quitter = messagebox.askquestion("Désirez-vous quitter le jeu ?", "Voulez-vous quitter le jeu maintenant ?") # Afficher une boîte de dialogue pour demander au joueur s'il souhaite arrêter de jouer
+    if quitter == "yes": # Si le joueur clique sur "Oui"
+        global running
+        running = False # Alors on arrête le jeu
+
+
+
 while running: # Tant que le jeu est en cours d'exécution
     for evenement in pygame.event.get(): # Pour chaque évènement intercepté pendant l'exécution de la boucle de jeu
         if evenement.type == pygame.QUIT: # Si le joueur désire quitter le jeu
-            running = False # Alors on arrête le jeu
+            quitter_jeu()
 
