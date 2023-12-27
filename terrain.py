764475592e627b0b12b9950e8e0584fc135c5eaf
Terence Le Thierry <terence.lethierry@protonmail.com>
2023-12-27 19:19:57 +0100

    Ajout terrain.py

---
+++ terrain.py
@@ -0,0 +1,19 @@
+# Terrain du jeu
+import pygame
+
+class Terrain():
+    "Terrain de jeu"
+    def __init__(self,largeur, hauteur, couleurs):
+        self.largeur = largeur # Largeur maximale du terrain
+        self.hauteur = hauteur # Hauteur maximale du terrain
+        self.couleurs = couleurs # Couleurs pour chaque carré du terrain
+        self.carre = Carre
+    
+
+
+
+class Carre(pygame.draw.rect):
+    def __init__():
+        pass
+
+
